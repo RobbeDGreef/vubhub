85efcc9cafb6d1b821d8e5c9f97fa8f258181278
Robbe De Greef <robbedegreef@gmail.com>
2020-10-10 00:01:50 +0200

    InfoHandler: Most settings are now moved to the infoHandler

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,11 +1,13 @@
 import "package:flutter/material.dart";
-import "classinfo.dart";
+import 'package:fvub/const.dart';
+import "infohandler.dart";
+import "const.dart";
 import "package:settings_ui/settings_ui.dart";
 
 class SettingsMenu extends StatefulWidget {
-  ClassInfo info;
+  InfoHandler info;
 
-  SettingsMenu(ClassInfo info) {
+  SettingsMenu(InfoHandler info) {
     this.info = info;
   }
 
@@ -14,34 +16,76 @@ class SettingsMenu extends StatefulWidget {
 }
 
 class _SettingsMenuState extends State<SettingsMenu> {
-  String dropDownVal;
-  ClassInfo info;
+  String dropDownColor;
+  String dropDownEduType = EducationData.keys.first;
+  String dropDownFac = EducationData[EducationData.keys.first].keys.first;
+  String dropDownEdu;
+  InfoHandler info;
 
-  _SettingsMenuState(ClassInfo info) {
+  List<String> getEducations() {
+    return EducationData[this.dropDownEduType][this.dropDownFac].keys.toList();
+  }
+
+  _SettingsMenuState(InfoHandler info) {
     this.info = info;
-    dropDownVal = info.getUserColorString();
+    this.dropDownColor = info.colorIntToString(info.getUserColor());
+    this.dropDownEduType = info.getUserEduType();
+  }
+
+  SettingsTile _settingDropdown(
+      String title, List<String> items, String item, Function(String) ptr) {
+    return SettingsTile(
+        title: title,
+        trailing: DropdownButton(
+            items: items.map<DropdownMenuItem<String>>((String item) {
+              return DropdownMenuItem(child: Text(item), value: item);
+            }).toList(),
+            value: item,
+            onChanged: ptr));
   }
 
   String tosay = "hello";
   Widget _buildSettings() {
     return SettingsList(sections: [
       SettingsSection(
-        title: "Common",
+        title: "User",
         tiles: [
-          SettingsTile(
-              title: "Color",
-              trailing: DropdownButton(
-                  items: <String>["blue", "orange"]
-                      .map<DropdownMenuItem<String>>((String item) {
-                    return DropdownMenuItem(child: Text(item), value: item);
-                  }).toList(),
-                  value: this.dropDownVal,
-                  onChanged: (String newval) {
-                    setState(() {
-                      this.dropDownVal = newval;
-                      this.info.updateColorFromString(newval);
-                    });
-                  })),
+          _settingDropdown("Color", ["blue", "orange"], this.dropDownColor, (String newval) {
+            setState(() {
+              this.dropDownColor = newval;
+              this.info.setUserColor(this.info.colorStringToInt(newval));
+            });
+          }),
+          _settingDropdown("Level of education", EducationData.keys.toList(), this.dropDownEduType,
+              (String newval) {
+            setState(() {
+              this.dropDownEduType = newval;
+              this.info.setUserEduType(newval);
+            });
+          }),
+          _settingDropdown(
+              "Faculty", EducationData[this.dropDownEduType].keys.toList(), this.dropDownFac,
+              (String newval) {
+            setState(() {
+              this.dropDownFac = newval;
+              this.info.setUserFac(newval);
+            });
+          }),
+          _settingDropdown("Education type", getEducations(), this.dropDownEdu, (String newval) {
+            print("new: $newval");
+            setState(() {
+              this.dropDownEdu = newval;
+              this.info.setUserEdu(newval);
+            });
+          }),
+          // TODO
+          _settingDropdown("Group", getEducations(), this.dropDownEdu, (String newval) {
+            print("new: $newval");
+            setState(() {
+              this.dropDownEdu = newval;
+              this.info.setUserEdu(newval);
+            });
+          }),
         ],
       )
     ]);
@@ -49,7 +93,6 @@ class _SettingsMenuState extends State<SettingsMenu> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("Settings")), body: _buildSettings());
+    return Scaffold(appBar: AppBar(title: Text("Settings")), body: _buildSettings());
   }
 }
