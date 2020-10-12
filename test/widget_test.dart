6721b5e98327b7963dc9c0e2c9bae60c0e26dbf4
Robbe De Greef <robbedegreef@gmail.com>
2020-10-12 11:25:58 +0200

    WidgetTest: Included correct main file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:fvub/_old.d';
+import 'package:fvub/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
