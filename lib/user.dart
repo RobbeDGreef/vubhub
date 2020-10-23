57c5fec3098074d6f4ac9aeefe4c43c59949447b
Robbe De Greef <robbedegreef@gmail.com>
2020-10-23 22:51:44 +0200

    User: created User object to hold all of users data

---
+++ lib/user.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+class User {
+  String name;
+  int id;
+  String email;
+  String locale;
+  String accessToken;
+  int rotationColor;
+  String educationType;
+  String faculty;
+  String education;
+  List<String> selectedGroups = [];
+
+  User.empty();
+  User({this.accessToken});
+
+  static final int stringListSize = 5;
+
+  User.fromJson(Map<String, dynamic> data) {
+    this.name = data['name'];
+    this.id = data['id'];
+    this.email = data['email'];
+    this.locale = data['locale'];
+    this.accessToken = data['accessToken'];
+    this.rotationColor = data['rotationColor'];
+    this.educationType = data['educationType'];
+    this.faculty = data['faculty'];
+    this.education = data['education'];
+    this.selectedGroups = [];
+    for (String s in data['selectedGroups']) {
+      this.selectedGroups.add(s);
+    }
+  }
+
+  String toJson() {
+    Map<String, dynamic> json = {
+      'name': this.name,
+      'id': this.id,
+      'email': this.email,
+      'locale': this.locale,
+      'accessToken': this.accessToken,
+      'rotationColor': this.rotationColor,
+      'educationType': this.educationType,
+      'faculty': this.faculty,
+      'education': this.education,
+      'selectedGroups': this.selectedGroups,
+    };
+
+    return jsonEncode(json);
+  }
+}
