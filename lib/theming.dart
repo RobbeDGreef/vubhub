c45d9890a7b3083280cb34c674de483a9d952cab
Robbe De Greef <robbedegreef@gmail.com>
2020-11-03 22:47:08 +0100

    Theming: Created a buildTheme function to build themeData

--- lib/theming.dart
+++ lib/theming.dart
@@ -1,4 +1,17 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
+
+import 'const.dart';
 
 final Color AlmostWhite = Color.fromRGBO(235, 238, 239, 1.0);
 final Color AlmostDark = Color.fromRGBO(72, 74, 82, 1.0);
+
+ThemeData buildTheme(bool light) {
+  return ThemeData(
+    primaryColor: VubOrange,
+    accentColor: light ? VubBlue : VubOrange,
+    unselectedWidgetColor: light ? VubBlue : VubOrange,
+    toggleableActiveColor: light ? VubBlue : VubOrange,
+    colorScheme: light ? ColorScheme.light() : ColorScheme.dark(),
+    scaffoldBackgroundColor: light ? AlmostWhite : AlmostDark,
+  );
+}
