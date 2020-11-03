2969f921668a7cbe3c2fd9363a390952101fd929
Robbe De Greef <robbedegreef@gmail.com>
2020-11-03 20:11:22 +0100

    HtmlParser: Created htmlParse() function for future custom html parsing

---
+++ lib/htmlParser.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_html/style.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+/// TODO: Create our own html parser, with one large selectable text widget and NO BUGS LIKE <p><a></a></p> is equal to a <p> tag like wth?????
+
+String processHtmlData(String data) {
+  return data;
+  print(data);
+  RegExp exp = new RegExp(r'(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+');
+  Iterable<RegExpMatch> matches = exp.allMatches(data);
+
+  int i = 0;
+  for (Match m in matches) {
+    String url = data.substring(m.start + i, m.end + i);
+    data = data.substring(0, m.start + i) +
+        " <a href=\"" +
+        url +
+        "\">" +
+        url +
+        "</a> " +
+        data.substring(m.end + i);
+    i += 17 + url.length;
+  }
+  return data;
+}
+
+Widget htmlParse(String data) {
+  data = processHtmlData(data);
+  return Html(
+    data: data,
+    onLinkTap: (String url) => launch(url),
+    customRender: {
+      'p': (context, child, attr, el) => SelectableText(el.text, style: TextStyle(fontSize: 18)),
+      'strong': (context, child, attr, el) =>
+          SelectableText(el.text, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+      'em': (context, child, attr, el) =>
+          SelectableText(el.text, style: TextStyle(fontStyle: FontStyle.italic, fontSize: 18)),
+    },
+    style: {
+      'div': Style(display: Display.BLOCK, fontSize: FontSize(18)),
+    },
+  );
+}
