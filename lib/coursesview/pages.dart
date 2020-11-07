47931d3545acd5376481232b535d5db352a27a28
Robbe De Greef <robbedegreef@gmail.com>
2020-11-08 00:33:01 +0100

    PagesView: Created a pagesview to view a list of pages per course

--- lib/coursesview/pages.dart
+++ lib/coursesview/pages.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../canvas/canvasapi.dart';
+import '../canvas/canvasobjects.dart';
+import '../htmlParser.dart';
+import 'pagedetails.dart';
+
 class PageView extends StatelessWidget {
   CanvasPage _page;
   Course _details;
@@ -25,6 +33,46 @@ class PageView extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+}
+
+class PagesView extends StatelessWidget {
+  Course _details;
+  CanvasApi _canvas;
+  Future<dynamic> _pagesFuture;
+
+  PagesView(Course details, CanvasApi canvas) {
+    this._details = details;
+    this._canvas = canvas;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageDetails(
+      color: this._details.color,
+      title: "Pages",
+      noDataText: "There are no pages for this course",
+      getData: () async {
+        List<dynamic> data = await this._canvas.get('api/v1/courses/${this._details.id}/pages');
+        List<CanvasPage> pages = [];
+        for (Map<String, dynamic> e in data) {
+          pages.add(CanvasPage(e));
+        }
+
+        return pages;
+      },
+      buildTile: (context, item) {
+        CanvasPage page = item;
+        return Card(
+          child: ListTile(
+            title: Text(page.name),
+            subtitle: Text(DateFormat("d MMMM yyyy 'at' HH:mm").format(page.created)),
+            onTap: () => Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => PageView(page, this._details, this._canvas))),
+          ),
+        );
+      },
     );
   }
 }
