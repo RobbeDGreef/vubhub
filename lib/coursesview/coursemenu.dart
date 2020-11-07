756998499cb0503082fe63a960e4641755210d51
Robbe De Greef <robbedegreef@gmail.com>
2020-11-08 00:39:42 +0100

    Moved quick coursemenu into a seperate file and class

--- lib/coursesview/coursemenu.dart
+++ lib/coursesview/coursemenu.dart
@@ -1,3 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+import '../canvas/canvasapi.dart';
+import '../canvas/canvasobjects.dart';
+import '../const.dart';
+import 'announcements.dart';
+import 'assignments.dart';
+import 'fileview.dart';
+import 'meetings.dart';
+import 'modules.dart';
+import 'notificationcounter.dart';
+import 'pagedetails.dart';
+import 'pages.dart';
+
 class CourseMenu extends StatelessWidget {
   Course _details;
   CanvasApi _canvas;
@@ -174,6 +190,118 @@ class CourseMenu extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class CourseMenuQuick extends StatelessWidget {
+  Course _details;
+  BuildContext context;
+  CanvasApi _canvas;
+
+  CourseMenuQuick(Course details, CanvasApi canvas) {
+    this._details = details;
+    this._canvas = canvas;
+  }
+
+  void _pushView(Widget Function() builder) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => builder()));
+  }
+
+  /// Build the quick menu button
+  Widget _buildNotificationButton({Icon icon, int amount, Color color, Function() onPressed}) {
+    /// Structure:
+    /// Padding
+    ///   - Stack to place the notification indicator on top of the iconbutton
+    ///     - IconButton
+    ///     - Positioned to place the card on the right of the stack (only if amount != 0)
+    ///       - Card
+    ///         - Text to display the notification amount
+
+    List<Widget> children = [
+      IconButton(
+        color: Colors.grey[600],
+        icon: icon,
+        iconSize: 35.0,
+        onPressed: onPressed,
+        padding: EdgeInsets.all(0),
+      )
+    ];
+
+    if (amount != 0) {
+      children.add(
+        Positioned(
+          right: 0,
+          child: NotificationCounter(this._details.color, amount),
+        ),
+      );
+    }
+
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Stack(
+        children: children,
+      ),
+    );
+  }
+
+  void _buildAnnouncements() {
+    _pushView(() => Announcements(this._details, this._canvas));
+  }
+
+  void _buildAssignments() {
+    _pushView(() => Assignments(this._details, this._canvas));
+  }
+
+  void _buildMeetings() {
+    _pushView(() => Meetings(details: this._details, canvas: this._canvas));
+  }
+
+  void _buildModules() {
+    _pushView(() => Modules(this._details, this._canvas));
+  }
+
+  void _buildAll() {
+    _pushView(() => CourseMenu(this._details, this._canvas));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    this.context = context;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildNotificationButton(
+          icon: Icon(Icons.campaign),
+          amount: this._details.unreadAnnouncementCount,
+          color: this._details.color,
+          onPressed: _buildAnnouncements,
+        ),
+        _buildNotificationButton(
+          icon: Icon(Icons.assignment),
+          amount: this._details.dueAssignments,
+          color: this._details.color,
+          onPressed: _buildAssignments,
+        ),
+        _buildNotificationButton(
+          icon: Icon(Icons.computer),
+          amount: this._details.curOngoingMeetings,
+          color: this._details.color,
+          onPressed: _buildMeetings,
+        ),
+        _buildNotificationButton(
+          icon: Icon(Icons.view_list),
+          amount: 0,
+          color: this._details.color,
+          onPressed: _buildModules,
+        ),
+        _buildNotificationButton(
+          icon: Icon(Icons.arrow_forward),
+          amount: 0,
+          color: this._details.color,
+          onPressed: _buildAll,
+        ),
+      ],
     );
   }
 }
